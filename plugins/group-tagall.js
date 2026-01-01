f3716433d60e859ce0f8b6baa1998a269ee88e56
livstsh <drkamran185@gmail.com>
2026-01-01 22:22:35 +0500

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -44,7 +44,7 @@ async (conn, mek, m, { from, participants, reply, isGroup, senderNumber, groupAd
             teks += `${randomEmoji} @${mem.id.split('@')[0]}\n`;
         }
 
-        teks += "└──✪ FATIMA ┃ MD ✪──";
+        teks += "└──✪ NAWAZ ┃ MD ✪──";
 
         conn.sendMessage(from, { text: teks, mentions: participants.map(a => a.id) }, { quoted: mek });
 
@@ -54,3 +54,4 @@ async (conn, mek, m, { from, participants, reply, isGroup, senderNumber, groupAd
     }
 });
 
+        
